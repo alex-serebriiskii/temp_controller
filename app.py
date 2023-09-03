a82414d194ed6f4f4f0e76922100d47698313e4c
Alex Serebriiskii <alex.serebriiskii@gmail.com>
2023-09-03 17:14:53 -0400

    Made webserver run on pi with one TC

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ from fastapi import FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from starlette.websockets import WebSocketDisconnect
-
+from breadboard import breadboard
+from max6675 import MAX6675, MAX6675Error
 app = FastAPI()
-
+# default example
+app.cs_pin = 24
+app.clock_pin = 23
+app.data_pin1 = 22
+#app.data_pin2 = 18
+app.units = "f"
+app.bb = breadboard(app.cs_pin, app.clock_pin)
+app.thermocouple1 = MAX6675(app.clock_pin, app.data_pin1, app.units)
+#app.thermocouple2 = MAX6675(app.clock_pin, app.data_pin2, app.units)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -14,26 +23,32 @@ connected_websockets = set()
 
 @app.get("/")
 async def index(request: Request):
+    print("bruh")
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     connected_websockets.add(websocket)
+    print("websocket connected")
     try:
         while True:
             try:
-                tc1 = thermocouple1.get()
-                tc2 = thermocouple2.get()
+                app.bb.wake()
+                print("bread is awake")
+                tc1 = app.thermocouple1.get()
+                #tc2 = app.thermocouple2.get()
+                app.bb.sleep()
             #number = random.randint(1, 100)
                 for ws in connected_websockets:
-                    await ws.send_text(str(number))
+                    await ws.send_text(str(tc1))
                 await asyncio.sleep(1)
                 
             except MAX6675Error as e:
                 tc = "Error: "+ e.value
                 running = False
                 print("tc: {}".format(tc))
+                await asyncio.sleep(1)
             
     except WebSocketDisconnect:
         connected_websockets.remove(websocket)
@@ -41,16 +56,7 @@ async def websocket_endpoint(websocket: WebSocket):
 if __name__ == "__main__":
     import uvicorn
     #set up pins and objects
-    # default example
-    cs_pin = 24
-    clock_pin = 23
-    data_pin1 = 22
-    data_pin2 = 18
-    units = "f"
-    bb = breadboard(cs_pin, clock_pin)
-    thermocouple1 = MAX6675(clock_pin, data_pin1, units)
-    thermocouple2 = MAX6675(clock_pin, data_pin2, units)
     try:
-        uvicorn.run(app, host="0.0.0.0", port=8000)
+        uvicorn.run(app)
     except KeyboardInterrupt:
-        breadboard.cleanup()
+        app.bb.cleanup()
--- breadboard.py
+++ breadboard.py
@@ -1,33 +1,34 @@
 #/usr/bin/python
 
+import RPi.GPIO as GPIO
 class breadboard(object):
-''' 
-Breadboard driver class for setting up multiple thermocouples on a breadboard
-'''
-def __init__(self, cs_pin, clock_pin, board = GPIO.BCM):
-    '''Initialize shared pins
+    ''' 
+    Breadboard driver class for setting up multiple thermocouples on a breadboard
+    '''
+    def __init__(self, cs_pin, clock_pin, board = GPIO.BOARD):
+        '''Initialize shared pins
         Parameters:
         - cs_pin:    Chip Select (CS) / Slave Select (SS) pin (Any GPIO)(sleep/wake sensors)  
         - clock_pin: Clock (SCLK / SCK) pin (Any GPIO)
         Note: 3.3v and ground are also shared pins across the breadboard
-    '''
-    self.cs_pin = cs_pin
-    self.clock_pin = clock_pin
-    self.board = board
+        '''
+        self.cs_pin = cs_pin
+        self.clock_pin = clock_pin
+        self.board = board
 
-    #Initialize board-specific GPIO
-    GPIO.setmode(self.board)
-    GPIO.setup(self.cs_pin, GPIO.OUT)
-    GPIO.setup(self.clock_pin, GPIO.OUT)
+        #Initialize board-specific GPIO
+        GPIO.setmode(self.board)
+        GPIO.setup(self.cs_pin, GPIO.OUT)
+        GPIO.setup(self.clock_pin, GPIO.OUT)
 
-    # Pull chip select high to make chip inactive
-    GPIO.output(self.cs_pin, GPIO.HIGH)
+        # Pull chip select high to make chip inactive
+        GPIO.output(self.cs_pin, GPIO.HIGH)
 
-    def wake():
+    def wake(self):
         # Toggle the chip to low for active
         GPIO.output(self.cs_pin, GPIO.LOW)
 
-    def sleep():
+    def sleep(self):
         # Toggle the chip to high for inactive
         GPIO.output(self.cs_pin, GPIO.HIGH)
         
@@ -36,7 +37,7 @@ def __init__(self, cs_pin, clock_pin, board = GPIO.BCM):
         GPIO.setup(self.cs_pin, GPIO.IN)
         GPIO.setup(self.clock_pin, GPIO.IN)
 
-class breadboard(Exception):
+class breadboardError(Exception):
      def __init__(self, value):
          self.value = value
      def __str__(self):
